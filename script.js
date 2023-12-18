65437349e49a85573fb53f5c5d3cacbdc8f40ac5
Kyureus <eduardoacostacarvajal@gmail.com>
2023-12-17 21:44:18 -0400

    first header implementation

---
+++ script.js
@@ -0,0 +1,31 @@
+console.log("hey! :)");
+
+const hamburgerButton = document.getElementById("hamburger-button");
+const dropdownContent = document.getElementById("dropdown-content");
+const closeMenuButton = document.getElementById("close-menu");
+const logo = document.getElementById("logo");
+const closeB = document.getElementById("close-menu");
+
+console.log(hamburgerButton);
+console.log(dropdownContent);
+console.log(logo);
+
+function dropdown() {
+    if(dropdownContent.style.display == "flex") {
+        logo.style.display = "block";
+    }
+    else {
+        logo.style.display = "none";
+        dropdownContent.style.display = "flex";
+        hamburgerButton.style.display = "none";  
+    }
+}
+function close() {
+    logo.style.display = "block";
+    dropdownContent.style.display = "none";
+    hamburgerButton.style.display = "flex";  
+}
+
+closeB.addEventListener('click', close);
+hamburgerButton.addEventListener('click', dropdown);
+
